605c6cbab70b9890a40407287facdb3659cc9d9b
allicedimello <allicedimello@disroot.org>
2025-08-21 15:38:00 -0300

    edit:Incrementando o código

---
+++ assets/script.js
@@ -0,0 +1,32 @@
+document.getElementById("form").addEventListener("submit", function(event) {
+    event.preventDefault();
+
+    let nomeHeroi = prompt("Digite o nome do herói:");
+    let xpHeroi = parseInt(prompt("Digite o XP do herói:"));
+    let nivelHeroi = "";
+
+    if (xpHeroi <= 1000){
+        nivelHeroi = "Ferro";
+    } else if (xpHeroi >= 1001 && xpHeroi <= 2000){
+        nivelHeroi = "Bronze";
+    } else if (xpHeroi >= 2001 && xpHeroi <= 5000){
+        nivelHeroi = "Prata";
+    } else if (xpHeroi >= 5001 && xpHeroi <= 7000){
+        nivelHeroi = "Ouro";
+    } else if (xpHeroi >= 7001 && xpHeroi <= 8000){
+        nivelHeroi = "Platina";
+    } else if (xpHeroi >= 8001 && xpHeroi <= 9000){
+        nivelHeroi = "Ascendente";
+    } else if (xpHeroi >= 9001 && xpHeroi <= 10000){
+        nivelHeroi = "Imortal";
+    } else if (xpHeroi >= 10001){
+        nivelHeroi = "Radiante";
+    } else {
+        parseInt(prompt("Valor inválido! Digite o XP do herói:"));
+    }
+
+    document.getElementById("resultado").textContent = `O Herói de nome ${nomeHeroi} está no nível ${nivelHeroi}`;
+
+    document.getElementById("form").reset();
+
+});
